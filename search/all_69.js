6c83e2592e22fcaa22fbfae0930c6a767db4e926
Luca Beldi <v.ronin@yahoo.it>
2013-11-20 17:50:10 +0000

    Finished documentation of student t

--- search/all_69.js
+++ search/all_69.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['independencecopula',['IndependenceCopula',['../class_multivariate_1_1_independence_copula.html',1,'Multivariate']]],
-  ['isvalid',['IsValid',['../class_multivariate_1_1_abstarct_distribution.html#a4c073c108279b10eff33aa11e988936e',1,'Multivariate::AbstarctDistribution::IsValid()'],['../class_multivariate_1_1t_distribution.html#a709cc27890bb812b881533be90461874',1,'Multivariate::tDistribution::IsValid()']]]
+  ['isvalid',['IsValid',['../class_multivariate_1_1_abstarct_distribution.html#a4c073c108279b10eff33aa11e988936e',1,'Multivariate::AbstarctDistribution']]]
 ];
--- search/functions_69.js
+++ search/functions_69.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['isvalid',['IsValid',['../class_multivariate_1_1_abstarct_distribution.html#a4c073c108279b10eff33aa11e988936e',1,'Multivariate::AbstarctDistribution::IsValid()'],['../class_multivariate_1_1t_distribution.html#a709cc27890bb812b881533be90461874',1,'Multivariate::tDistribution::IsValid()']]]
+  ['isvalid',['IsValid',['../class_multivariate_1_1_abstarct_distribution.html#a4c073c108279b10eff33aa11e988936e',1,'Multivariate::AbstarctDistribution']]]
 ];
